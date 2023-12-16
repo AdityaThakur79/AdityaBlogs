c520a3ec310097c1629e356221ada43ea353637b
Thakur Aditya <adityathakurbusiness@gmail.com>
2023-12-16 15:19:51 +0530

    NoPage Removed

--- src/pages/nopage/Nopage.jsx
+++ src/pages/nopage/Nopage.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+// import React from 'react'
 
-const Nopage = () => {
-    return (
-        <div>
-            Nopage
-        </div>
-    )
-}
+// const Nopage = () => {
+//     return (
+//         <div>
+//             Nopage
+//         </div>
+//     )
+// }
 
-export default Nopage
+// export default Nopage;
